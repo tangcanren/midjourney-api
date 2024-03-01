f6824f69ad4db208c08353fac693a9d0f3b3ec85
唐粲人 <tangcanren@metrodata.tech>
2024-03-01 14:30:01 +0800

    更新docker repo

--- lib/api/discord.py
+++ lib/api/discord.py
@@ -6,7 +6,7 @@ import aiohttp
 
 from lib.api import CHANNEL_ID, USER_TOKEN, GUILD_ID
 from util.fetch import fetch, fetch_json, FetchMethod
-
+#test
 TRIGGER_URL = "https://discord.com/api/v9/interactions"
 UPLOAD_ATTACHMENT_URL = f"https://discord.com/api/v9/channels/{CHANNEL_ID}/attachments"
 SEND_MESSAGE_URL = f"https://discord.com/api/v9/channels/{CHANNEL_ID}/messages"
